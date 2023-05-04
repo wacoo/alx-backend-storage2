4e004344fde92317aac715eaee7cfa3c1c7ecd7c
wac <wabaham9@gmail.com>
2023-05-04 20:05:14 +0000

    tasks 8-101

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,3 +1,3 @@
 -- create view need_meeting that lists all students that score unde 80
 -- and no last_meeting or more than a month
-CREATE VIEW AS SELECT name FROM students WHERE score < 80 OR last_meeting = NULL; --  OR last_meeting
+CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 OR last_meeting = NULL OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH);
